a56e5b2a27becc753e7fb356e5a415c318b69157
slaoqaltttttttttt <slaoqalt2@gmail.com>
2025-06-16 11:35:58 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: 'MTM4MzIyODgwMzQyOTQzMzUwNg.GrrdZi.xg2FJ0uwDQXm02Qzi0dL_ntavA245dwTdRPgNk',
+  token: '',
   userToken: 'MTM4MjQ4MDY1Njc4NDk1MzQ2Nw.GZX7T4.K5DU4cUAAOULQ2aaJkKD58xFzCHLk6VmWiIMaA',
   prefix: ['js!', 'stock'],
 };
