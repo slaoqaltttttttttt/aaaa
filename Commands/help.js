9488f192c1c8be4246a080cafe197964a367f836
slaoqaltttttttttt <slaoqalt2@gmail.com>
2025-06-18 23:14:40 -0300

    Create help.js

---
+++ Commands/help.js
@@ -0,0 +1,22 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: 'help',
+  description: 'Mostra a lista de comandos e como usar o bot de stock',
+  async execute(botClient, message, args) {
+    const helpEmbed = new EmbedBuilder()
+      .setTitle('Stock help')
+      .setColor(0x9B59B6) // Roxo
+      .setDescription(
+        '`s!setup:`\n' +
+        'Configura o canal em que o stock vai ser enviado.\n' +
+        'Exemplo: `s!setup #stock`\n\n' +
+        '`s!pings`\n' +
+        'Configure os pings de certos itens em stock, Shard Mitico, Galo Lendario, etc.\n\n' +
+        '`s!stock`\n' +
+        'Veja o stock atual do asura shop.\n'
+      );
+
+    await message.reply({ embeds: [helpEmbed] });
+  }
+};
