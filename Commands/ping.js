1e13b1be5993336330971a46580e2b381a92c7d9
slaoqaltttttttttt <slaoqalt2@gmail.com>
2025-07-02 23:07:16 -0300

    Update ping.js

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,6 +1,36 @@
+const { EmbedBuilder } = require('discord.js')
+
+/* COMANDO: ping */
 module.exports = {
   name: 'ping',
   async execute(client, message, args) {
-    await message.channel.send('Pong!');
+    try {
+      const embedCalculando = new EmbedBuilder()
+        .setTitle('🏓 Pong!')
+        .setColor(0x00BFFF)
+        .setDescription('Calculando...')
+      const sent = await message.channel.send({ embeds: [embedCalculando] })
+
+      const apiPing = sent.createdTimestamp - message.createdTimestamp
+      const gatewayPing = Math.round(client.ws.ping)
+
+      const pingEmbed = new EmbedBuilder()
+        .setTitle('🏓 Pong!')
+        .setColor(0x00BFFF)
+        .setDescription(
+          `API Ping: ${apiPing}ms\nGateway Ping: ${gatewayPing}ms`
+        )
+
+      await sent.edit({ embeds: [pingEmbed] })
+    } catch (error) {
+      const errorEmbed = new EmbedBuilder()
+        .setTitle('Erro')
+        .setDescription(
+          'Ocorreu um erro ao executar este comando.' +
+          (error?.message ? `\nMotivo: ${error.message}` : '')
+        )
+        .setColor(0x8B0000)
+      await message.channel.send({ embeds: [errorEmbed] })
+    }
   }
-};
+}
