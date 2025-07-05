5b22d62480b682e66b4b7e47099d9bd7dbf13081
slaoqaltttttttttt <slaoqalt2@gmail.com>
2025-07-05 19:34:10 -0300

    intents

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ const botClient = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,     
+    GatewayIntentBits.GuildPresences   
   ]
 })
 const userClient = new UserClient()
