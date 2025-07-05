7108b188d3504c9a5ffe77be62f831c4a11de42f
slaoqaltttttttttt <slaoqalt2@gmail.com>
2025-07-05 16:02:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  postgresConnectionString: 'postgresql://postgres:150150c@@db.whhdrodrxepfqoxymfoq.supabase.co:5432/postgres',
   token: '',
   userToken: '',
   prefix: ['s!', 'stock'],
